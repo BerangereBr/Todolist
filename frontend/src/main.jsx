0c6322159c76407871a671ad2e69be080394dd74
Berangere Br <brunberangere@gmail.com>
2025-12-18 15:48:25 +0100

    installation de tailwind

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { UserContext } from './contexts/UserContext'
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
