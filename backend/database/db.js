af804d62937d8c6d25079ae2b2f539f5b35186aa
Berangere Br <brunberangere@gmail.com>
2026-01-05 17:42:37 +0100

    correction conflit

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: process.env.PG_user,
-    host: process.env.PG_host,
-    database: process.env.PG_ddb,
-    password: process.env.PG_password,
-    port: process.env.PG_port,
+    // user: process.env.PG_user,
+    // host: process.env.PG_host,
+    // database: process.env.PG_ddb,
+    // password: process.env.PG_password,
+    // port: process.env.PG_port,
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false // obligatoire pour Render
