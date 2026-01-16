7964347c305096d0b8aafc7f122f076f8ec368c0
Berangere Br <brunberangere@gmail.com>
2026-01-16 14:09:06 +0100

    ajout un composant de chargement

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import { useUser } from "../hooks/useUser";
 import { useNavigate } from "react-router-dom";
+import Loading from "./Loading";
 
 function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
     const [error, setError] = useState("");
 
     const navigate = useNavigate();
@@ -13,6 +15,7 @@ function Login() {
     const handlesubmit = async (e) => {
         e.preventDefault();
         setError();
+        setLoading(true);
 
         try {
             await login(email, password);
@@ -20,7 +23,13 @@ function Login() {
         } catch (err) {
             console.log(err);
             setError("Email ou mot de passe incorrect.")
+        } finally {
+            setLoading(false);
         }
+    }
+
+    if (loading) {
+        return <Loading />;
     }
     return (
         <form onSubmit={handlesubmit} className="login-container">
@@ -34,16 +43,20 @@ function Login() {
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                     required
-                    autoComplete="username" />
+                    autoComplete="username"
+                    disabled={loading} />
                 <label htmlFor="login-password">Mot de passe :</label>
                 <input id="login-password"
                     type="password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                     required
-                    autoComplete="current-password" />
+                    autoComplete="current-password"
+                    disabled={loading} />
                 {error && <p>{error}</p>}
-                <button type="submit">Se connecter</button>
+                <button type="submit" disabled={loading}>
+                    Se connecter
+                </button>
             </div>
         </form>
     )
