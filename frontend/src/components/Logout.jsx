f5157e6f3548db715953b905ecc04bc874ecaf1b
Berangere Br <brunberangere@gmail.com>
2026-01-26 16:08:12 +0100

    changement icon deconnexion window<576px

--- frontend/src/components/Logout.jsx
+++ frontend/src/components/Logout.jsx
@@ -1,8 +1,21 @@
 import { useNavigate } from "react-router-dom";
 import { useUser } from "../hooks/useUser";
+import { useState, useEffect } from "react";
 import '../styles/logout.scss';
+import logoutIcon from '../assets/images/logout.png';
 
 function Logout() {
+    const [sizeWindow, setSizeWindow] = useState(window.innerWidth > 576);
+    useEffect(() => {
+        const handleResize = () => {
+            setSizeWindow(window.innerWidth > 576);
+        };
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     const { logout } = useUser();
     const navigate = useNavigate();
     const handleLogout = () => {
@@ -11,7 +24,7 @@ function Logout() {
     }
     return (
         <button onClick={handleLogout} className="btn-logout">
-            Se déconnecter
+            {sizeWindow ? 'Se déconnecter' : <img src={logoutIcon} alt="logout" className="icon-logout"></img>}
         </button>
     )
 }
