850698067e04dbd22e180d78134ffc2f1f6d2ab6
Berangere Br <brunberangere@gmail.com>
2026-02-26 18:58:39 +0100

    ajout serverless

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,3 +1,4 @@
-// api/index.js
-const app = require('../app'); // ton app Express
-module.exports = app;
+const app = require('../app');
+const serverless = require('serverless-http');
+
+module.exports = serverless(app);
