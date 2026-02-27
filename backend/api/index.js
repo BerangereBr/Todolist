cbfb6089576bce61ed049466eecf34c55aa7b629
Berangere Br <brunberangere@gmail.com>
2026-02-27 13:24:01 +0100

    retour test

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,14 +1,5 @@
-const express = require('express');
-const app = express();
-
-app.use(express.json());
-
+const app = require('../app');
 const serverless = require('serverless-http');
-
-app.get('/test', (req, res) => {
-    console.log('✅ /test route appelée !');
-    res.status(200).send('Test route OK');
-});
 
 console.log("✅ api/index.js chargé et exécuté");
 module.exports = serverless(app);
