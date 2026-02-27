0888922dbdd16014596f3cbdb42455dd680e5e62
Berangere Br <brunberangere@gmail.com>
2026-02-27 11:48:21 +0100

    test serverless

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,5 +1,8 @@
 const app = require('./app');
 const serverless = require('serverless-http');
+const handler = serverless(app);
 
-console.log("✅ api/index.js chargé et exécuté");
+handler({ httpMethod: 'GET', path: '/health' }, {
+    succeed: (response) => console.log(response)
+});
 module.exports = serverless(app);
