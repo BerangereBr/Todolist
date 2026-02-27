20fa28f3f1b2090803d528145788f64b0824c0b3
Berangere Br <brunberangere@gmail.com>
2026-02-27 12:32:16 +0100

    test nouveau app a une route

--- backend/api/app.js
+++ backend/api/app.js
@@ -1,32 +1,32 @@
 const express = require('express');
-const cors = require('cors');
-const authRouter = require('../routes/authRouter');
-const todolistsRouter = require('../routes/todolistRouter');
-const todoRouter = require('../routes/todoRouter')
+// const cors = require('cors');
+// const authRouter = require('../routes/authRouter');
+// const todolistsRouter = require('../routes/todolistRouter');
+// const todoRouter = require('../routes/todoRouter')
 const app = express();
 
 app.use(express.json());
 
-const allowedOrigins = [
-    'https://todolist-ten-pearl-25.vercel.app',
-    'http://localhost:5173'
-];
+// const allowedOrigins = [
+//     'https://todolist-ten-pearl-25.vercel.app',
+//     'http://localhost:5173'
+// ];
 
-app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true
-}));
+// app.use(cors({
+//     origin: function (origin, callback) {
+//         if (!origin || allowedOrigins.includes(origin)) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error('Not allowed by CORS'));
+//         }
+//     },
+//     credentials: true
+// }));
 
 
-app.use('/api/auth', authRouter);
-app.use('/api/todolists', todolistsRouter);
-app.use('/api/todolists/:todolist_id/todos', todoRouter);
+// app.use('/api/auth', authRouter);
+// app.use('/api/todolists', todolistsRouter);
+// app.use('/api/todolists/:todolist_id/todos', todoRouter);
 app.get('/test', (req, res) => {
     console.log('✅ /test route appelée !');  // ça doit apparaître dans les logs Vercel
     res.status(200).send('Test route OK');
