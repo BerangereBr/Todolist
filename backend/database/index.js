ffd89fe058bb940e35d5fb33225e5625919a702f
Berangere Br <brunberangere@gmail.com>
2026-02-27 10:31:12 +0100

    essaie log lien supabase

--- backend/database/index.js
+++ backend/database/index.js
@@ -4,5 +4,6 @@ const supabase = createClient(
     process.env.SUPABASE_URL,
     process.env.SUPABASE_SERVICE_ROLE_KEY
 );
-
+console.log("SUPABASE_URL:", process.env.SUPABASE_URL);
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
 module.exports = supabase;
