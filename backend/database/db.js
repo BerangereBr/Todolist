ef5f4b1221762dc261cbec172d86dc2408065226
Berangere Br <brunberangere@gmail.com>
2026-02-27 11:29:31 +0100

    changement pour essaie lien bdd

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,25 +1,25 @@
-// const { Pool } = require('pg');
+const { Pool } = require('pg');
 
-// const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === 'production';
 
-// const pool = new Pool(
-//     isProduction
-//         ? {
-//             connectionString: process.env.DATABASE_URL,
-//             ssl: { rejectUnauthorized: false }, // Supabase
-//         }
-//         : {
-//             user: process.env.PG_user,
-//             host: process.env.PG_host,
-//             database: process.env.PG_ddb,
-//             password: process.env.PG_password,
-//             port: process.env.PG_port,
-//             ssl: false
-//         }
-// );
+const pool = new Pool(
+    isProduction
+        ? {
+            connectionString: process.env.DATABASE_URL,
+            ssl: { rejectUnauthorized: false }, // Supabase
+        }
+        : {
+            user: process.env.PG_user,
+            host: process.env.PG_host,
+            database: process.env.PG_ddb,
+            password: process.env.PG_password,
+            port: process.env.PG_port,
+            ssl: false
+        }
+);
 
-// pool.query('SELECT NOW()')
-//     .then(res => console.log('DB OK:', res.rows[0]))
-//     .catch(err => console.error('DB ERROR:', err));
+pool.query('SELECT NOW()')
+    .then(res => console.log('DB OK:', res.rows[0]))
+    .catch(err => console.error('DB ERROR:', err));
 
-// module.exports = pool;
+module.exports = pool;
--- backend/database/index.js
+++ backend/database/index.js
@@ -1,20 +1,20 @@
-const { createClient } = require('@supabase/supabase-js');
+// const { createClient } = require('@supabase/supabase-js');
 
-const supabase = createClient(
-    process.env.SUPABASE_URL,
-    process.env.SUPABASE_SERVICE_ROLE_KEY
-);
+// const supabase = createClient(
+//     process.env.SUPABASE_URL,
+//     process.env.SUPABASE_SERVICE_ROLE_KEY
+// );
 
-module.exports = async (req, res) => {
-    console.log("SUPABASE_URL:", process.env.SUPABASE_URL);
+// module.exports = async (req, res) => {
+//     console.log("SUPABASE_URL:", process.env.SUPABASE_URL);
 
-    // Exemple d'appel à la base (facultatif mais utile pour logs)
-    const { data, error } = await supabase.from('todolist').select('*').limit(1);
+//     // Exemple d'appel à la base (facultatif mais utile pour logs)
+//     const { data, error } = await supabase.from('todolist').select('*').limit(1);
 
-    if (error) console.error('SUPABASE ERROR:', error);
+//     if (error) console.error('SUPABASE ERROR:', error);
 
-    res.status(200).json({
-        message: 'API WORKS',
-        test: data || null
-    });
-};
+//     res.status(200).json({
+//         message: 'API WORKS',
+//         test: data || null
+//     });
+// };
