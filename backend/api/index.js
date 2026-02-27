a5d149a4fc963688402642b173330fcc786726d1
Berangere Br <brunberangere@gmail.com>
2026-02-27 12:13:40 +0100

    supression handler

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,4 +2,4 @@ const app = require('./app');
 const serverless = require('serverless-http');
 
 console.log("✅ api/index.js chargé et exécuté");
-module.exports.handler = serverless(app);
+module.exports = serverless(app);
