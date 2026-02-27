ffaa8b642f0aec41243a538e46e434ace65eb282
Berangere Br <brunberangere@gmail.com>
2026-02-27 14:57:46 +0100

    test

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,4 +1,12 @@
-const app = require('../app');
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
 
 console.log("✅ api/index.js chargé et exécuté");
+app.get('/test', (req, res) => {
+    console.log('✅ /test route appelée !');
+    res.status(200).send('Test route OK');
+});
 module.exports = app;
