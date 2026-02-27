380f3d9708e16588f2f00f6fab6862a4ae3a718f
Berangere Br <brunberangere@gmail.com>
2026-02-27 10:42:04 +0100

    essaie lien vercel

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,4 +1,4 @@
-// const app = require('../app');
+const app = require('../app');
 // const serverless = require('serverless-http');
 module.exports = (req, res) => {
     res.status(200).json({ message: "API WORKS" });
