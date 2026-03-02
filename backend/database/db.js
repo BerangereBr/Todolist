d2ac64b15107dd9ea4be63348106835c795661ec
Berangere Br <brunberangere@gmail.com>
2026-03-02 15:11:29 +0100

    changement pool en fonction

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,19 +1,26 @@
 const { Pool } = require('pg');
 
-const pool = new Pool(
-    process.env.DATABASE_URL
-        ? {
-            connectionString: process.env.DATABASE_URL,
-            ssl: { rejectUnauthorized: false }
-        }
-        : {
-            user: process.env.PG_user,
-            host: process.env.PG_host,
-            database: process.env.PG_ddb,
-            password: process.env.PG_password,
-            port: process.env.PG_port,
-            ssl: false
-        }
-);
+let pool;
 
-module.exports = pool;
+function getPool() {
+    if (!pool) {
+        pool = new Pool(
+            process.env.DATABASE_URL
+                ? {
+                    connectionString: process.env.DATABASE_URL,
+                    ssl: { rejectUnauthorized: false }
+                }
+                : {
+                    user: process.env.PG_user,
+                    host: process.env.PG_host,
+                    database: process.env.PG_ddb,
+                    password: process.env.PG_password,
+                    port: process.env.PG_port,
+                    ssl: false
+                }
+        );
+    }
+    return pool;
+}
+
+module.exports = getPool;
