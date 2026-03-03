ce20a300ac3dcb99ffc3a386f4f0bec7c2189b02
Berangere Br <brunberangere@gmail.com>
2026-03-03 12:42:50 +0100

    modif route router

--- backend/app.js
+++ backend/app.js
@@ -29,10 +29,10 @@ app.use(cors({
 }));
 
 
-app.use('/api/auth', authRouter);
-app.use('/api/todolists', todolistsRouter);
-app.use('/api/todolists/:todolist_id/todos', todoRouter);
-app.get('/api/health', (req, res) => {
+app.use('/auth', authRouter);
+app.use('/todolists', todolistsRouter);
+app.use('/todolists/:todolist_id/todos', todoRouter);
+app.get('/health', (req, res) => {
     console.log('✅ /test route appelée !');
     res.status(200).send('Test route OK');
 });
